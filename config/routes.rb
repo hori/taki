74c9f4eb151f29d8de5f60ec232587f65b0817ac
kozyty <kozyty@gmail.com>
2013-11-14 11:51:48 +0900

    Add AdminUser

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Taki::Application.routes.draw do
+  
+  
+  devise_for :admin_users
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
