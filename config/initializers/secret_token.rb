c7d321978cab38c3b3c15b7bca6d80fad327dd67
kozyty <kozyty@gmail.com>
2013-11-21 19:27:44 +0900

    not ignored secret_token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taki::Application.config.secret_key_base = '96b2eb2e1bc01e9b23977bea296c83ff9a60aa1f0d8381f14c8955b45a261365d36006c16f21aad3d18e0e449cf357c5be6ebbb7dc5bde97bffa2d519035a1df'
