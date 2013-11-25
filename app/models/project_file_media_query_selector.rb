0b8f89cc5bff9aac965be62b5f96e754b1223710
kozyty <kozyty@gmail.com>
2013-11-25 16:04:05 +0900

    fixup! Model#ProjectFileMediaQuerySelector relations

--- app/models/project_file_media_query_selector.rb
+++ app/models/project_file_media_query_selector.rb
@@ -1,4 +1,6 @@
 class ProjectFileMediaQuerySelector < ActiveRecord::Base
   belongs_to :project_file_media_query
-  has_many :project_file_media_query_selector_properties, :project_file_media_query_selector_similarities
+
+  has_many :project_file_media_query_selector_properties
+  has_many :project_file_media_query_selector_similarities
 end
