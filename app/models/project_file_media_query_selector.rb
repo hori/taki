abcad1aa8b5d36766be65efb268be7f8dacb1943
kozyty <kozyty@gmail.com>
2013-11-25 16:54:07 +0900

    fixup! belongs_to hotfix.

--- app/models/project_file_media_query_selector.rb
+++ app/models/project_file_media_query_selector.rb
@@ -1,5 +1,5 @@
 class ProjectFileMediaQuerySelector < ActiveRecord::Base
-  belongs_to :project_file_media_query, :as => :media_query
+  belongs_to :media_query, :class_name => 'ProjectFileMediaQuery', :foreign_key => :project_file_media_query_id
 
   has_many :project_file_media_query_selector_properties, :as => :properties
   has_many :project_file_media_query_selector_similarities, :as => :similarities
--- app/models/project_file_media_query_selector_property.rb
+++ app/models/project_file_media_query_selector_property.rb
@@ -1,3 +1,3 @@
 class ProjectFileMediaQuerySelectorProperty < ActiveRecord::Base
-  belongs_to :project_file_media_query_selector, :as => :selector
+  belongs_to :selector, :class_name => 'ProjectFileMediaQuerySelector', :foreign_key => :project_file_media_query_selector_id
 end
