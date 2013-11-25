48e9e494d438a36e6d15f07a5c854aae659f3631
kozyty <kozyty@gmail.com>
2013-11-25 15:45:26 +0900

    fixup! Model#ProjectFileMediaQuerySelectorSimilarity

--- app/models/project_file_media_query_selector_similarity.rb
+++ app/models/project_file_media_query_selector_similarity.rb
@@ -1,4 +1,4 @@
 class ProjectFileMediaQuerySelectorSimilarity < ActiveRecord::Base
-  has_one :current_selector, foreign_key => "current_selector_id", class_name => "ProjectFileMediaQuerySelector"
-  has_one :compared_selector, foreign_key => "compared_selector_id", class_name => "ProjectFileMediaQuerySelector"
+  has_one :current_selector, :foreign_key => "current_selector_id", :class_name => "ProjectFileMediaQuerySelector"
+  has_one :compared_selector, :foreign_key => "compared_selector_id", :class_name => "ProjectFileMediaQuerySelector"
 end
