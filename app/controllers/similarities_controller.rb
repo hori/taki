922a093d6531a81bb05a481603670b911a27f98b
hori <snowbell.mac@gmail.com>
2013-11-28 12:48:17 +0900

    [draft] add SimilaritiesController

---
+++ app/controllers/similarities_controller.rb
@@ -0,0 +1,23 @@
+require 'pp'
+class SimilaritiesController < TakiController
+
+  def index
+    calc_similarity 1,2
+  end
+
+  def show
+  end
+
+  private
+  def calc_similarity(selector_id_a,selector_id_b)
+    properties_a = ProjectFileMediaQuerySelectorProperty.where(project_file_media_query_selector_id: selector_id_a).all
+    properties_b = ProjectFileMediaQuerySelectorProperty.where(project_file_media_query_selector_id: selector_id_b).all
+
+
+    for property in properties do
+      pp property
+    end
+
+  end
+
+end
