6b65f863158799f3402626de74729fcd8c5e1bb8
hori <snowbell.mac@gmail.com>
2013-11-28 20:02:24 +0900

    [draft] create combination

--- app/controllers/similarities_controller.rb
+++ app/controllers/similarities_controller.rb
@@ -1,7 +1,8 @@
 class SimilaritiesController < TakiController
 
   def index
-    calc_similarity 1,2
+    calc_similarities 3
+    #calc_similarity 1,2
   end
 
   def show
@@ -15,6 +16,22 @@ class SimilaritiesController < TakiController
     diff = properties_a - properties_b
 
 
+  end
+
+  def calc_similarities(file_id)
+    selector_ids = ProjectFileSelector.where(project_file_id: file_id).pluck(:id)
+    combinations = []
+    selector_ids.each_with_index do |id_a, index|
+      for id_b in selector_ids[index+1..-1] do
+        combinations << %W|#{id_a} #{id_b}|
+      end
+    end
+
+    # @todo parallelization
+    for selector_ids in combinations do
+      properties_a = ProjectFileSelectorProperty.where(project_file_selector_id: selector_ids[0])
+      properties_b = ProjectFileSelectorProperty.where(project_file_selector_id: selector_ids[1])
+    end
   end
 
 end
