1fa140faf2c2fc0f7bfba59ce23cbeaa11e3456a
kozyty <kozyty@gmail.com>
2013-11-29 09:56:31 +0900

    fixup! ProjectFileSelectorProperty#create

--- app/models/project_file.rb
+++ app/models/project_file.rb
@@ -18,16 +18,27 @@ class ProjectFile < ActiveRecord::Base
   def css_parser
     parse = CssParser::Parser.new
     parse.class_eval { attr_reader :rules }
+
     parse.load_uri!(self.attachment.path)
     parse.rules.each do |rule|
       project_file_selector = self.project_file_selectors.create({
         :name => rule[:rules].selectors.join(', '),
         :raw => rule[:rules].to_s,
       })
+
       project_file_media_query = self.project_file_media_queries.create({
         :name => rule[:media_types].join(' '),
         :project_file_selector_id => project_file_selector.id,
       }) unless rule[:media_types].blank?
+
+      rule[:rules].class_eval { attr_reader :declarations }
+      rule[:rules].declarations.keys.each_with_index do |name, i|
+        project_file_selector.project_file_selector_properties.create({
+          :name => name,
+          :value => rule[:rules].declarations.values[i][:value],
+          :is_important => rule[:rules].declarations.values[i][:is_important]
+        })
+      end
     end
   end
 end
--- app/models/project_file_selector_property.rb
+++ app/models/project_file_selector_property.rb
@@ -1,3 +1,4 @@
 class ProjectFileSelectorProperty < ActiveRecord::Base
+  attr_accessible :name, :value, :is_important, :line, :start
   belongs_to :project_file_selector
 end
